f2d24e99f19824cb6d19627da14a7a42654c60a3
kagomee12 <--global>
2025-08-25 15:59:49 +0700

    Add JWT middleware for authorization handling

---
+++ app/middleware/jwt_middleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"net/http"
+	"shoplink/app/pkg"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWTMiddleware(JWTService pkg.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Authorization header is missing"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			c.Abort()
+			return
+		}
+
+		token, err := JWTService.ValidateToken(parts[1])
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", token.ID)
+		c.Set("username", token.Username)
+
+		c.Next()
+	}
+}
+
